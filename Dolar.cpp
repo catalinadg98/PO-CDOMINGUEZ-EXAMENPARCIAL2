937147da418be736187c2dbae404971d0ffe9988
catalinadg98 <35185749+catalinadg98@users.noreply.github.com>
2018-04-02 21:01:11 -0500

    Update Dolar.cpp

--- Dolar.cpp
+++ Dolar.cpp
@@ -2,64 +2,79 @@
 // Created by Catalina Domínguez on 4/2/18.
 //
 
+#include "Peso.h"
 #include "Dolar.h"
 
-Dolar::Dolar() {
-    this->dls = 0;
+Peso::Peso() {
+    this->mxn = 0;
 }
 
-Dolar::Dolar(double dls){
-    this->dls=dls;
+Peso::Peso(double mxn){
+    this->mxn=mxn;
 }
 
-Dolar operator + (const Dolar &n1, const Dolar &n2){
-    Dolar totaldls;
-    totaldls = n1.dls + n2.dls;
-    return totaldls;
+Peso operator + (const Peso &p1, const Peso &p2){
+    Peso totalmxn;
+    totalmxn = p1.mxn + p2.mxn;
+    return totalmxn;
 }
 
-Dolar operator - (const Dolar &n1, const Dolar &n2){
-    Dolar totaldls;
-    totaldls = n1.dls - n2.dls;
-    return totaldls;
+Peso operator - (const Peso &p1, const Peso &p2){
+    Peso totalmxn;
+    totalmxn = p1.mxn - p2.mxn;
+    return totalmxn;
 }
 
-bool operator < (const Dolar &n1, const Dolar &n2){
-    bool res = (n1 < n2);
+/*
+Peso operator + (const Dolar &p1, const Peso &p2){
+    Peso totalmxn;
+    totalmxn.mxn = ((p1.dls * 18.5) + p2.mxn);
+    return totalmxn;
+}
+ */
+
+Peso operator + (const Peso &p1, const Dolar &p2){
+    Peso totalmxn;
+    totalmxn.mxn = (p1.mxn + (p2.dls * 18.5));
+    return totalmxn;
+}
+
+bool operator < (const Peso &p1, const Peso &p2){
+    bool res = (p1 < p2);
     return res;
     //True
 }
 
-bool operator > (const Dolar &n1, const Dolar &n2){
-    bool res = (n1 > n2);
+bool operator > (const Peso &p1, const Peso &p2){
+    bool res = (p1 > p2);
     return res;
     //True
 }
 
-bool operator <= (const Dolar &n1, const Dolar &n2){
-    bool res = (n1 <= n2);
+bool operator <= (const Peso &p1, const Peso &p2){
+    bool res = (p1 <= p2);
     return res;
     //True
 }
 
-bool operator >= (const Dolar &n1, const Dolar &n2){
-    bool res = (n1 >= n2);
+bool operator >= (const Peso &p1, const Peso &p2){
+    bool res = (p1 >= p2);
     return res;
     //True
 }
 
-bool operator == (const Dolar &n1, const Dolar &n2){
-    bool res = (n1 == n2);
+bool operator == (const Peso &p1, const Peso &p2){
+    bool res = (p1 == p2);
     return res;
     //True
 }
 
-bool operator != (const Dolar &n1, const Dolar &n2){
-    bool res = (n1 != n2);
+bool operator != (const Peso &p1, const Peso &p2){
+    bool res = (p1 != p2);
     return res;
     //True
 }
 
-std::string Dolar::to_String() {
-    return "$" + std::to_string(dls) + " USD";
+std::string Peso::to_String() {
+    return "$" + std::to_string(mxn) + " MXN";
 }
